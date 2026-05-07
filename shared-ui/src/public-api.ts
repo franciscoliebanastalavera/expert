9e11c92a3103ff675d1b0cb8c160d8638ed5a90e
FranciscoLiebanas <francisco.liebanas@nter.es>
2026-05-07 09:01:00 +0200

    chore(shared-ui): drop 5 unused components, legacy modal, includes pipe and dead models

--- shared-ui/src/public-api.ts
+++ shared-ui/src/public-api.ts
@@ -1,5 +1,3 @@
-export * from './lib/modal/modal.component';
-
 export * from './lib/safe-html/safe-html.pipe';
 
 export * from './lib/cap-button/cap-button.component';
@@ -24,27 +22,15 @@ export * from './lib/cap-modal/cap-modal.component';
 
 export * from './lib/cap-input/cap-input.component';
 
-export * from './lib/cap-card/cap-card.component';
-
 export * from './lib/cap-tabs/cap-tabs.component';
 export * from './lib/cap-tabs/cap-tab/cap-tab.component';
 
 export * from './lib/cap-tooltip/cap-tooltip.component';
 
-export * from './lib/cap-select/cap-select.component';
-
-export * from './lib/cap-checkbox/cap-checkbox.component';
-
-export * from './lib/cap-datepicker/cap-datepicker.component';
-
-export * from './lib/cap-switch/cap-switch.component';
-
 export * from './lib/core/pipes/iban.pipe';
-export * from './lib/core/pipes/includes.pipe';
 
 export * from './lib/core/directives/click-outside.directive';
 
-export * from './lib/core/models/components.models';
 export * from './lib/core/types/components.types';
 
 export * from './lib/cap-footer/cap-footer.component';
