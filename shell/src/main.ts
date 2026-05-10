babd11f5f9f036e37faab378bf420e3832703c88
FranciscoLiebanas <francisco.liebanas@nter.es>
2026-05-10 11:49:17 +0200

    feat(shell): introduce environments and log bootstrap errors

--- shell/src/main.ts
+++ shell/src/main.ts
@@ -1,10 +1,18 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { APP_LOCALE_ID } from './app/language.constants';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 registerLocaleData(localeEs, APP_LOCALE_ID);
 
-bootstrapApplication(AppComponent, appConfig).catch(() => {});
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error('CapitalFlow shell bootstrap failed', err),
+);
